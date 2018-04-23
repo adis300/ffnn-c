63257664e4369d2916f2136700095e702e45540b
Disi A <adis@live.cn>
2018-04-23 00:22:47 -0400

    Network structures.

--- ffnn.c
+++ ffnn.c
@@ -9,7 +9,17 @@
  *  https://www.mathworks.com/matlabcentral/profile/authors/3734620-disi-a
  */
 
+#include "ffnn.h"
 #include <math.h>
+#include <string.h>
+
+#define LOOKUP_SIZE 4096
+#define SIGMOID_CUTOFF 45.0
+
+const double SIGMOID_DOM_MIN = -20.0;
+const double SIGMOID_DOM_MAX = 20.0;
+
+double sigmoid_lookup[LOOKUP_SIZE];
 
 // Activation functions
 double inline ffnn_activation_linear(double x) { return x;}
@@ -22,8 +32,27 @@ double inline ffnn_activation_relu(double x) {
 }
 
 double inline ffnn_activation_sigmoid(double x) {
-    if (x < -45.0) return 0;
-    if (x > 45.0) return 1;
+    if (x < -SIGMOID_CUTOFF) return 0;
+    if (x > SIGMOID_CUTOFF) return 1;
     return 1.0 / (1 + exp(-x));
+}
+
+NetworkLayer* create_layer(int numberOfNodes, int inputLength, double* weights, double * biases, const char* activation){
+    
+    return 0;
+}
+
+/*
+void create_ffnn_sigmoid_lookup() {
+    const double f = (SIGMOID_DOM_MAX - SIGMOID_DOM_MIN) / LOOKUP_SIZE;
+    double interval = LOOKUP_SIZE / (SIGMOID_DOM_MAX - SIGMOID_DOM_MIN);
+    for (int i = 0; i < LOOKUP_SIZE; ++i) {
+        sigmoid_lookup[i] = ffnn_activation_sigmoid(SIGMOID_DOM_MIN + f * i);
+    }
+}
+*/
+
+double * layer_output (int col, int row, double * input, double * grid, double * bias) {
+
 }
 
--- ffnn.h
+++ ffnn.h
@@ -17,10 +17,50 @@
 extern "C" {
 #endif
 
+typedef double (*ffnn_activation_func)(double z);
+
+typedef struct {
+
+    ffnn_activation_func activation;
+
+    int numberOfNodes;
+
+    int inputLength;
+
+    double *weights; // (numberOfNodes * inputLength) A grid, row1, row2, row3 ... to represent a matrix
+
+    double *biases; // (NumberOfNodes) A Vector, bias1, bias2, bias3 ... to represent a vector
+
+    double *output; // (NumberOfNodes) A Vector, bias1, bias2, bias3 ... to represent a vector
+
+} NetworkLayer;
+
+typedef struct {
+    /* Total number of weights, and size of weights buffer. */
+    int output_length;
+
+    /* Total number of neurons + inputs and size of output buffer. */
+    int input_length;
+
+    /* All weights (total_weights long). */
+    double * output;
+
+    /* Stores input array and output of each neuron (total_neurons long). */
+    int layer_length;
+
+    /* Stores delta of each hidden and output neuron (total_neurons - inputs long). */
+    NetworkLayer * layers;
+
+} Network;
+
 double ffnn_activation_sigmoid(double x);
 double ffnn_activation_threshold(double x);
 double ffnn_activation_linear(double x);
 double ffnn_activation_relu(double x);
+
+NetworkLayer* create_layer(int numberOfNodes, int inputLength, double* weights, double * biases, const char* activation);
+
+double * run (Network* network, double * input);
 
 #ifdef __cplusplus
 }
