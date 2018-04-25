a841faf430f740c9b9ce37fefda6c2499eab183e
Disi A <adis@live.cn>
2018-04-25 15:46:00 -0400

    Update examples.

--- example.c
+++ example.c
@@ -3,12 +3,15 @@
 #include <stdlib.h>
 #include <math.h>
 
-static char * JSON_NETWORK = "{\"layerSizes\":[3,2,2],\"activations\":[\"relu\",\"relu\"],\"weights\":[{\"col\": 3,\"row\": 2,\"grid\":[1,1, 1.0, 1.0, 1.0, 1.0]},{\"col\": 2,\"row\": 2,\"grid\":[1,1,1,1]}],\"biases\":[{\"vector\" :[ 0.25, 0.25]},{\"vector\" :[ 0.5, 0.5]}]}";
+static char * JSON_NETWORK_ACTIVATION_BY_LAYERS = "{\"layerSizes\":[3,2,2],\"activations\":[\"relu\",\"relu\"],\"weights\":[{\"col\": 3,\"row\": 2,\"grid\":[1,1, 1.0, 1.0, 1.0, 1.0]},{\"col\": 2,\"row\": 2,\"grid\":[1,1,1,1]}],\"biases\":[{\"vector\" :[ 0.25, 0.25]},{\"vector\" :[ 0.5, 0.5]}]}";
 
-int main() {   
-    printf("========= FFNN example =========\n");
+static char * JSON_NETWORK_UNIVERSAL_ACTIVATION = "{\"layerSizes\":[3,2,2],\"activation\":\"sigmoid\",\"weights\":[{\"col\": 3,\"row\": 2,\"grid\":[1,1, 1.0, 1.0, 1.0, 1.0]},{\"col\": 2,\"row\": 2,\"grid\":[1,1,1,1]}],\"biases\":[{\"vector\" :[ 0.25, 0.25]},{\"vector\" :[ 0.5, 0.5]}]}";
 
-    Network* network = create_network(JSON_NETWORK);
+void activation_by_layer(){
+
+    printf("----- EXAMPLE1: Activation by layer example -----\n");
+
+    Network* network = create_network(JSON_NETWORK_ACTIVATION_BY_LAYERS);
 
     double * input = (double *) alloca(3 * sizeof(double));
     input[0] = 1.0; input[1] = 1.0; input[2] = 1.0;
@@ -17,8 +20,30 @@ int main() {
     printf("Network response:%lf,%lf \n", output[0],output[1]);
 
     printf("Network saved response:%lf,%lf \n", network -> output[0], network -> output[1]);
+    printf("----- Activation by layer done. -----\n\n");
+}
 
-    printf("========= Done. =========\n");
+void activation_universal(){
+
+    printf("----- EXAMPLE2:Activation universal example -----\n");
+
+    Network* network = create_network(JSON_NETWORK_UNIVERSAL_ACTIVATION);
+
+    double * input = (double *) alloca(3 * sizeof(double));
+    input[0] = 1.0; input[1] = 1.0; input[2] = 1.0;
+    double * output = run_network(network, input);
+
+    printf("Network response:%lf,%lf \n", output[0],output[1]);
+
+    printf("Network saved response:%lf,%lf \n", network -> output[0], network -> output[1]);
+    printf("----- Activation universal done. -----\n\n");
+}
+
+int main() {   
+    printf("========= FFNN example =========\n\n");
+    activation_by_layer();
+    activation_universal();
+    printf("========= Done. =========\n\n");
     return 0;
 }
 
